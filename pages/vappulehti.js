7688c74bb43b0ab3309a672b2f1c9752c02e398c
Juho Järvi <juho.jaervi@gmail.com>
2025-04-16 14:51:19 +0300

    chore: added content to vappulehti for testing

--- pages/vappulehti.js
+++ pages/vappulehti.js
@@ -3,20 +3,20 @@ import Layout from "../components/Layout";
 const articles = [
   {
     id: 1,
-    title: "Ensimmäinen artikkeli",
-    content: "Tämä on ensimmäisen artikkelin sisältö.",
-    image: "https://placehold.co/600x400", // Placeholder-kuva
+    title: "Rento ruusu",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo justo a nunc efficitur vehicula in non sem. Vestibulum lorem libero, convallis ac condimentum id, lobortis nec nisl. Maecenas erat urna, elementum in erat non, sagittis auctor dolor. Sed sit amet est et turpis posuere pellentesque quis sed purus. Proin elementum faucibus dignissim. Aliquam molestie faucibus ante, eu cursus lectus cursus et. Nam a viverra leo, in posuere dui. Aenean enim mauris, scelerisque sit amet magna vel, fringilla blandit lorem. Cras ac quam at lacus imperdiet faucibus sed at dui. Vivamus quam lorem, eleifend ac ex et, euismod feugiat massa.",
+    image: "rentoruusu.JPG", // Placeholder-kuva
   },
   {
     id: 2,
-    title: "Toinen artikkeli",
-    content: "Tämä on toisen artikkelin sisältö.",
-    image: "https://placehold.co/600x400", // Placeholder-kuva
+    title: "Perustamiskokous",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo justo a nunc efficitur vehicula in non sem. Vestibulum lorem libero, convallis ac condimentum id, lobortis nec nisl. Maecenas erat urna, elementum in erat non, sagittis auctor dolor. Sed sit amet est et turpis posuere pellentesque quis sed purus. Proin elementum faucibus dignissim. Aliquam molestie faucibus ante, eu cursus lectus cursus et. Nam a viverra leo, in posuere dui. Aenean enim mauris, scelerisque sit amet magna vel, fringilla blandit lorem. Cras ac quam at lacus imperdiet faucibus sed at dui. Vivamus quam lorem, eleifend ac ex et, euismod feugiat massa.",
+    image: "perustamis.jpg", // Testikuva
   },
   {
     id: 3,
-    title: "Kolmas artikkeli",
-    content: "Tämä on kolmannen artikkelin sisältö.",
+    title: "joo joku artikkeli",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo justo a nunc efficitur vehicula in non sem. Vestibulum lorem libero, convallis ac condimentum id, lobortis nec nisl. Maecenas erat urna, elementum in erat non, sagittis auctor dolor. Sed sit amet est et turpis posuere pellentesque quis sed purus. Proin elementum faucibus dignissim. Aliquam molestie faucibus ante, eu cursus lectus cursus et. Nam a viverra leo, in posuere dui. Aenean enim mauris, scelerisque sit amet magna vel, fringilla blandit lorem. Cras ac quam at lacus imperdiet faucibus sed at dui. Vivamus quam lorem, eleifend ac ex et, euismod feugiat massa.",
     image: "https://placehold.co/600x400", // Placeholder-kuva
   },
 ];
