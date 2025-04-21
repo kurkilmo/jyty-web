c21050860c3019335d0a42a69fe5793556f36141
Juho Järvi <juho.jaervi@gmail.com>
2025-04-21 13:35:18 +0300

    chore: added icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/global.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <link rel='icon' href="logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
